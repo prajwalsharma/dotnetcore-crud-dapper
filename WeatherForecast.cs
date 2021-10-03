4e0730a9c9b3fbcd69ad4bfe8f8037e388bb96bc
prjwl4 <prjwl4@gmail.com>
2021-10-03 16:43:27 +0530

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CRUD_NET_Core_Web_API_Dapper
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
