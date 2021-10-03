9f0bde9918ae45e91f27faaeedac72f316ba9c26
prjwl4 <prjwl4@gmail.com>
2021-10-03 19:23:03 +0530

    Created Employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_NET_Core_Web_API_Dapper.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+        public string CITY { get; set; }
+        public string DEPARTMENT { get; set; }
+        public string GENDER { get; set; }
+        public string ACTIVE_STATUS { get; set; }
+    }
+}
